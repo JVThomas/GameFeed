18750c3b834b9848a28891efcecce001298cec4a
Justin Thomas <jthomas0209@gmail.com>
2016-06-28 10:23:21 -0400

    Moved Twitch ID to header request for incoming August changes

--- app/services/twitch_service.rb
+++ app/services/twitch_service.rb
@@ -3,14 +3,14 @@ class TwitchService
   def channels(title)
     @resp = Faraday.get 'https://api.twitch.tv/kraken/search/streams' do |req|
       req.params["query"] = '"' + "#{title}" + '"'
-      req.params["client_id"] = ENV["TWITCH_API"]
+      req.headers["client-id"] = ENV["TWITCH_API"]
     end
     @result = JSON.parse(@resp.body)
   end
 
   def pagination(link)
     @resp = Faraday.get link do |req|
-      req.params["client_id"] = ENV["TWITCH_API"]
+      req.headers["client-id"] = ENV["TWITCH_API"]
     end
     @result = JSON.parse(@resp.body)
   end
