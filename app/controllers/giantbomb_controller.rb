ab7c5409ad7e720251368d327f635fff34028002
Justin Thomas <jthomas0209@gmail.com>
2016-08-14 12:17:25 -0400

    modified twitch service spec

--- app/controllers/giantbomb_controller.rb
+++ app/controllers/giantbomb_controller.rb
@@ -1,5 +1,6 @@
 class GiantbombController < ApplicationController
   before_action :set_gb_service
+  
   def games
     @results = @giantbomb.games(params[:query])
     render json: @results 
--- spec/services/twitch_service_spec.rb
+++ spec/services/twitch_service_spec.rb
@@ -10,12 +10,24 @@ describe 'TwitchService' do
 		expect(@streams["streams"].length).not_to eql(0)
 	end
 
-	it 'returns links for the previous 10 and following 10 links' do
-		next_link = @streams["_links"]["next"]
-		new_streams = @twitch.pagination(next_link)
+	context 'pagination' do 
+		
+		before(:each) do 
+			@next_link = @streams["_links"]["next"]
+			@next_streams = @twitch.pagination(@next_link)
+		end
+		
+		it 'returns links for the next 10 links' do
+			expect(@next_streams["streams"].length).not_to eql(0)
+			expect(@next_streams).not_to eq(@streams)
+		end
 
-		expect(new_streams["streams"].length).not_to eql(0)
-		expect(new_streams).not_to eq(@streams)
+		it 'returns links for the previous 10 links' do
+			prev_link = @next_streams["_links"]["prev"]
+			@prev_streams = @twitch.pagination(prev_link)
+			
+			expect(@prev_streams["streams"].length).not_to eql(0)
+			expect(@prev_streams).not_to eq(@next_streams)
+		end
 	end
-	
 end
