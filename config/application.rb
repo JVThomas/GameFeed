742e624e627340d3cc4eaa883da2cb397b2f7ead
Justin Thomas <jthomas0209@gmail.com>
2016-12-06 15:12:29 -0500

    another whitelist fix

--- config/application.rb
+++ config/application.rb
@@ -33,7 +33,7 @@ module GameFeed
 
     config.middleware.insert_before 0, "Rack::Cors" do
       allow do
-        origins 'http://gamefeedapp.herokuapp.com'
+        origins 'http://gamefeedapp.herokuapp.com/'
         resource '*', :headers => :any, :methods => [:get, :post, :options, :delete, :put]
       end
     end
