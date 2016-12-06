5a44a3fb126b3e70ab7a4fbe3251b91b31df36ad
Justin Thomas <jthomas0209@gmail.com>
2016-12-06 13:22:40 -0500

    heroku fix

--- config/application.rb
+++ config/application.rb
@@ -33,7 +33,7 @@ module GameFeed
 
     config.middleware.insert_before 0, "Rack::Cors" do
       allow do
-        origins '*'
+        origins 'gamefeedapp.herokuapp.com'
         resource '*', :headers => :any, :methods => [:get, :post, :options, :delete, :put]
       end
     end
